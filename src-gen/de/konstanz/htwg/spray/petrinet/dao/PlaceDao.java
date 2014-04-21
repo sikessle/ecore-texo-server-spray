8580e96fdd57397e3a39f0daf4ab9a4f40f23b3a
jahoefne <jahoefne@htwg-konstanz.de>
2014-04-21 22:58:10 +0200

    Big speed up of generic remove method.

--- src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
+++ src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
@@ -1,24 +1,14 @@
 package de.konstanz.htwg.spray.petrinet.dao;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
+import org.eclipse.emf.common.util.Reflect;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.xmi.impl.StringSegment;
-import org.eclipse.emf.texo.model.ModelObject;
-import org.eclipse.emf.texo.model.ModelResolver;
 import org.eclipse.emf.texo.server.store.BaseDao;
-import org.eclipse.emf.texo.server.store.DaoRegistry;
-import org.eclipse.emf.texo.server.store.EntityManagerProvider;
 
-import de.konstanz.htwg.spray.petrinet.InputArc;
-import de.konstanz.htwg.spray.petrinet.Net;
-import de.konstanz.htwg.spray.petrinet.OutputArc;
 import de.konstanz.htwg.spray.petrinet.PetriNetModelPackage;
 import de.konstanz.htwg.spray.petrinet.Place;
-import de.konstanz.htwg.spray.petrinet.Token;
-
-import java.lang.reflect.*;
 /**
  * The Dao implementation for the model object '<em><b>Place</b></em>'.
  * 
@@ -36,32 +26,26 @@ public class PlaceDao extends BaseDao<Place> {
 
 	/**
 	 * Clears all the references on an object before deleting it.
-	 * Uses reflection to be as generic as possible/easy to generate,
-	 * but trades in potential speedup compared to a specialized version
+	 * Uses reflection to be as generic as possible/easy to generate
 	 */
 	@Override
 	public void remove(Place objectToDelete) {
-		
 		if(objectToDelete == null){
 			throw new IllegalArgumentException();
 		}
 		
-		String className = objectToDelete.getClass().getSimpleName();
-		List<Object> referers = this.getReferingObjects(objectToDelete, Integer.MAX_VALUE, true);
+		final String objectClass = objectToDelete.getClass().getSimpleName();
+		final List<Object> referers = this.getReferingObjects(objectToDelete, Integer.MAX_VALUE, true);
 		
 		// clear the references
 		for (Object referer : referers) {
-			Method[] refererMethods = referer.getClass().getMethods();
-			for(Method m : refererMethods){
-				if(m.getName().equals("removeFrom"+className)){
-					try {
-						m.invoke(referer,objectToDelete);
-					} catch (Exception e) {
-						e.printStackTrace();
-						return;
-					}
+				try {
+					Method removeFromMethod = referer.getClass().
+							getMethod("removeFrom"+objectClass,objectToDelete.getClass());
+					removeFromMethod.invoke(referer,objectToDelete);
+				} catch (Exception e){
+					System.out.println(e);
 				}
-			}
 		}
 		super.remove(objectToDelete);
 	}
