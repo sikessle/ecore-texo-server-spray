77a081a96b4efaca94fde526c63c4e0a471f0091
jahoefne <jahoefne@htwg-konstanz.de>
2014-04-21 19:29:50 +0200

    Dao Method for clearing db foreign refs is working

--- src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
+++ src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
@@ -33,48 +33,38 @@ public class PlaceDao extends BaseDao<Place> {
 		return Place.class;
 	}
 
-	
 	@Override
+	// clears references of Place t before removing the object.
+	// To solve the db foreign key constraint problem.
+	//
+	// Note: to remove a reference from a Object A to a Object B
+	// we must NOT call B.removeFrom..(A), but A.removeFrom...(B)!
+	//
+	// generation of this code is not difficult, we must somehow find out 
+	// which refs can exist -- use reflection for that?
 	public void remove(Place t){
 	   System.out.println("Deleting Place: "+t.getDb_Id());
 	
 	   // list contains the objects that refer to Place t
-	   List<Object> list = this.getReferingObjects(t, Integer.MAX_VALUE, true);
-	   System.out.println("Threre are "+list.size()+" references on t");
+	   List<Object> referingObjects = this.getReferingObjects(t, Integer.MAX_VALUE, true);
+	   System.out.println("Threre are "+referingObjects.size()+" references on t");
 	   System.out.println("The references to this object are:");
 	   
-	   // t.removeFrom.. does not seem to work
-	   for(Object o : list){
+	   for(Object o : referingObjects){
 		  try{
 		   InputArc i = (InputArc)o;
-		   t.removeFromInputArc(i);
-		   EntityManagerProvider.getInstance().getEntityManager().persist(i);
-		   EntityManagerProvider.getInstance().getEntityManager().flush();
-		   EntityManagerProvider.getInstance().getEntityManager().refresh(i);
-		   
+		   System.out.println(i.removeFromPlace(t));
 		   System.out.println("\t"+i.toString()+" "+i.getDb_Id());
 		  }catch(Exception e){}
-		  
 		  try{
 		   OutputArc i = (OutputArc)o;
-		   t.removeFromOutputArc(i);
-		   EntityManagerProvider.getInstance().getEntityManager().persist(i);
-		   EntityManagerProvider.getInstance().getEntityManager().flush();
-		   EntityManagerProvider.getInstance().getEntityManager().refresh(i);
-
+		   System.out.println(i.removeFromPlace(t));
 		   System.out.println("\t"+i.toString()+" "+i.getDb_Id());
 		  }catch(Exception e){}
-		  
-		  try{			  
-		   Token i = (Token)o;
-		   t.removeFromToken(i);
-		   EntityManagerProvider.getInstance().getEntityManager().persist(i);
-		   EntityManagerProvider.getInstance().getEntityManager().flush();
-		   EntityManagerProvider.getInstance().getEntityManager().refresh(i);
-
-		   System.out.println("\t"+i.toString()+" "+i.getDb_Id());
-		  }catch(Exception e){} 
 		}
+	   
+	   // we cleared the refs, can delete the object itself now
+	   super.remove(t);
 	}
 	
 	@Override
