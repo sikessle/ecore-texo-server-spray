edfa5d453d69d731ab6c48c476078bca4d167688
jahoefne <jahoefne@htwg-konstanz.de>
2014-04-21 21:46:10 +0200

    Made custom remove method generic using reflection

--- src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
+++ src-gen/de/konstanz/htwg/spray/petrinet/dao/PlaceDao.java
@@ -2,9 +2,9 @@ package de.konstanz.htwg.spray.petrinet.dao;
 
 import java.util.List;
 
-
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.xmi.impl.StringSegment;
 import org.eclipse.emf.texo.model.ModelObject;
 import org.eclipse.emf.texo.model.ModelResolver;
 import org.eclipse.emf.texo.server.store.BaseDao;
@@ -18,6 +18,7 @@ import de.konstanz.htwg.spray.petrinet.PetriNetModelPackage;
 import de.konstanz.htwg.spray.petrinet.Place;
 import de.konstanz.htwg.spray.petrinet.Token;
 
+import java.lang.reflect.*;
 /**
  * The Dao implementation for the model object '<em><b>Place</b></em>'.
  * 
@@ -33,49 +34,46 @@ public class PlaceDao extends BaseDao<Place> {
 		return Place.class;
 	}
 
+	/**
+	 * Clears all the references on an object before deleting it.
+	 * Uses reflection to be as generic as possible/easy to generate,
+	 * but trades in potential speedup compared to a specialized version
+	 */
 	@Override
-	// clears references of Place t before removing the object.
-	// To solve the db foreign key constraint problem.
-	//
-	// Note: to remove a reference from a Object A to a Object B
-	// we must NOT call B.removeFrom..(A), but A.removeFrom...(B)!
-	//
-	// generation of this code is not difficult, we must somehow find out 
-	// which refs can exist -- use reflection for that?
-	public void remove(Place t){
-	   System.out.println("Deleting Place: "+t.getDb_Id());
-	
-	   // list contains the objects that refer to Place t
-	   List<Object> referingObjects = this.getReferingObjects(t, Integer.MAX_VALUE, true);
-	   System.out.println("Threre are "+referingObjects.size()+" references on t");
-	   System.out.println("The references to this object are:");
-	   
-	   for(Object o : referingObjects){
-		  try{
-		   InputArc i = (InputArc)o;
-		   System.out.println(i.removeFromPlace(t));
-		   System.out.println("\t"+i.toString()+" "+i.getDb_Id());
-		  }catch(Exception e){}
-		  try{
-		   OutputArc i = (OutputArc)o;
-		   System.out.println(i.removeFromPlace(t));
-		   System.out.println("\t"+i.toString()+" "+i.getDb_Id());
-		  }catch(Exception e){}
+	public void remove(Place objectToDelete) {
+		
+		if(objectToDelete == null){
+			throw new IllegalArgumentException();
 		}
-	   
-	   // we cleared the refs, can delete the object itself now
-	   super.remove(t);
+		
+		String className = objectToDelete.getClass().getSimpleName();
+		List<Object> referers = this.getReferingObjects(objectToDelete, Integer.MAX_VALUE, true);
+		
+		// clear the references
+		for (Object referer : referers) {
+			Method[] refererMethods = referer.getClass().getMethods();
+			for(Method m : refererMethods){
+				if(m.getName().equals("removeFrom"+className)){
+					try {
+						m.invoke(referer,objectToDelete);
+					} catch (Exception e) {
+						e.printStackTrace();
+						return;
+					}
+				}
+			}
+		}
+		super.remove(objectToDelete);
 	}
-	
+
 	@Override
-	public void remove(List<Place> t){
-		for(Place p : t){
+	public void remove(List<Place> t) {
+		for (Place p : t) {
 			this.remove(p);
 		}
 		return;
 	}
-	
-	
+
 	/**
 	 * @generated
 	 */
